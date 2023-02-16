9088a2c8c7cd31f1132fcf17b212d089d9e7e72b
linzell <linzellart@gmail.com>
2023-02-16 14:40:57 +0100

    ✅ Add Item enum

---
+++ src/classes/enums/itemTypeEnum.ts
@@ -0,0 +1,13 @@
+/**
+ * Get the item type from the item type code
+ */
+// eslint-disable-next-line no-shadow
+enum ItemTypeEnum {
+  FOLDER = 'Folder',
+  PAGE = 'Page',
+  TAG = 'Tag',
+  USER = 'User',
+  TEAM = 'Team',
+}
+
+export default ItemTypeEnum;
