a9e5835c2b76703e502c9b353c25149133a71adf
linzell <linzellart@gmail.com>
2023-02-27 23:32:12 +0100

    📝 Add commentary for futur

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -19,7 +19,9 @@ const store = configureStore({
   },
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
+// Inferred type
 export type AppDispatch = typeof store.dispatch;
 
 export default store;
