050985e730456148b67b900c1341971bd7cc70de
linzell <linzellart@gmail.com>
2023-03-15 22:08:30 +0100

    ✨ Start job in user form input

---
+++ src/components/settings/userIdentity/userForm.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import User from '#/user';
+
+export default function userForm(
+  props: {
+    user: User,
+  },
+) {
+  return (
+    <Card sx={{ width: { xs: '85vw', md: '60vw' } }}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
