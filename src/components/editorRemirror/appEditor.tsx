0e0fd4c4172d7e8c4fcc24869a13bae42f573a68
linzell <linzellart@gmail.com>
2023-03-23 23:46:09 +0100

    ✨ YJS Editor

--- src/components/editorRemirror/appEditor.tsx
+++ src/components/editorRemirror/appEditor.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
-import { EditorComponent, useRemirror, Remirror } from '@remirror/react';
-import extensions from './editorManager';
-import Menu from './editorMenu';
+import { YjsExtension } from '@remirror/extension-yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import * as Y from 'yjs';
+import {
+  Remirror,
+  ThemeProvider,
+  useRemirror,
+} from '@remirror/react';
 
-import 'remirror/styles/all.css';
+const room = 'test-placeholder-yjs';
+const ydoc = new Y.Doc();
+const provider = new WebrtcProvider(room, ydoc);
+
+const extensions = () => [
+  new YjsExtension({ getProvider: () => provider, disableUndo: true }),
+];
 
 export default function appEditor() {
-  const { manager, state } = useRemirror({
+  const { manager } = useRemirror({
     extensions,
-    content: '<p>I love <b>Remirror</b></p>',
-    selection: 'start',
-    stringHandler: 'html',
+    core: { excludeExtensions: ['history'] },
   });
   return (
     <div className='remirror-theme'>
-      <Remirror manager={manager} initialContent={state}>
-        <EditorComponent />
-        <Menu />
-      </Remirror>
+      <ThemeProvider>
+        <Remirror manager={manager} autoFocus autoRender='end'>
+        </Remirror>
+      </ThemeProvider>
     </div>
   );
 }
